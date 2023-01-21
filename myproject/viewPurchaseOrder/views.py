32a96101a2d5506c182a0988f648963520ea071a
waiee <waiee_z@yahoo.com>
2023-01-22 07:59:10 +0800

    test

--- myproject/viewPurchaseOrder/views.py
+++ myproject/viewPurchaseOrder/views.py
@@ -8,6 +8,20 @@ from app.models import QuotationItem, PurchaseOrder
 from django.http import HttpRequest
 
 @login_required
+
+def viewPo(request):
+    po_list = PurchaseOrder.objects.all()
+
+    return render(
+        request,
+        'viewPurchaseOrder/viewPo.html',
+        {
+            'title':'View Purchase Order',
+            'year':datetime.now().year,
+            'po_list' : po_list,
+        }
+    )
+
 def searchPo(request):
     if request.method == "POST":
         searched = request.POST['searched']
@@ -40,20 +54,6 @@ def selectPo(request, purchaseOrderID):
     else:
         return render(request, 
         'viewPurchaseOrder/selectPo.html',{})
-
-
-def viewPo(request):
-    po_list = PurchaseOrder.objects.all()
-
-    return render(
-        request,
-        'viewPurchaseOrder/viewPo.html',
-        {
-            'title':'View Purchase Order',
-            'year':datetime.now().year,
-            'po_list' : po_list,
-        }
-    )
 
 def backtoHome(request):
     """Renders the home page."""
