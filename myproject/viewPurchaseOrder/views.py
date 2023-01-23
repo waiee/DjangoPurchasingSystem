b2b5a4081dfad2bdc5a742240dc11e14f97f0a84
waiee <waiee_z@yahoo.com>
2023-01-23 23:24:40 +0800

    commit

--- myproject/viewPurchaseOrder/views.py
+++ myproject/viewPurchaseOrder/views.py
@@ -56,7 +56,7 @@ def approvePo(request):
     #getting PO ive selected
     currentPo = PurchaseOrder.objects.filter(purchaseOrderID=request.POST.get("purchaseOrder"))
     print(currentPo)
-    currentPo.update(poStatus='Approved')
+    currentPo.update(poStatus='Pending')
 
     #data after
     print(dataP)
